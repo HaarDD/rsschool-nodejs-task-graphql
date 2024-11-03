d15bc3f33396bbf88205507ab1f427bb88dcb524
HaarDD <maxgalitskiy@gmail.com>
2024-11-03 20:52:00 +0300

    feat: enhance GraphQL plugin with additional DataLoaders and validation

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -3,9 +3,10 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 import { FastifyPluginAsync } from 'fastify';
-import { graphql } from 'graphql';
+import { execute, parse, validate } from 'graphql';
 import { schema } from './schema.js';
-import { createUserLoader } from './dataloader.js';
+import { createMemberTypeLoader, createPostLoader, createProfileLoader, createSubscribedToLoader, createSubscribersLoader, createUserLoader } from './dataloader.js';
+import depthLimit from 'graphql-depth-limit';
 
 declare module 'fastify' {
   interface FastifyInstance {
@@ -17,14 +18,34 @@ const plugin: FastifyPluginAsync = async (fastify) => {
   fastify.decorate('userLoader', createUserLoader(fastify.prisma));
 
   fastify.post('/', async (request, reply) => {
-    fastify.log.info('GraphQL endpoint hit');
-    const { query, variables } = request.body as { query: string; variables?: Record<string, any> };
+    const { query, variables } = request.body as 
+    { 
+      query: string; 
+      variables?: Record<string, any> 
+    };
 
-    const result = await graphql({
+    const document = parse(query);
+    const validationErrors = validate(schema, document, [depthLimit(5)]);
+
+    if (validationErrors.length > 0) {
+      reply.send({ errors: validationErrors });
+      return;
+    }
+
+    const result = await execute({
       schema,
-      source: query,
+      document,
       variableValues: variables,
-      contextValue: { prisma: fastify.prisma, userLoader: fastify.userLoader },
+      contextValue: {
+        prisma: fastify.prisma,
+        userLoader: createUserLoader(fastify.prisma),
+        postLoader: createPostLoader(fastify.prisma),
+        profileLoader: createProfileLoader(fastify.prisma),
+        memberTypeLoader: createMemberTypeLoader(fastify.prisma),
+        subscribedToLoader: createSubscribedToLoader(fastify.prisma),
+        subscribersLoader: createSubscribersLoader(fastify.prisma),
+        log: fastify.log,
+      },
     });
 
     reply.send(result);
