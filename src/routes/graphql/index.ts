3dabaf7c6ef39af96036d0bba9fc7e08822f8711
HaarDD <maxgalitskiy@gmail.com>
2024-11-03 16:50:34 +0300

    feat: refactor GraphQL plugin to use Fastify and add user loader

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,22 +1,33 @@
-import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+import { FastifyPluginAsync } from 'fastify';
 import { graphql } from 'graphql';
+import { schema } from './schema.js';
+import { createUserLoader } from './dataloader.js';
 
-const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
-  const { prisma } = fastify;
+declare module 'fastify' {
+  interface FastifyInstance {
+    userLoader: ReturnType<typeof createUserLoader>;
+  }
+}
 
-  fastify.route({
-    url: '/',
-    method: 'POST',
-    schema: {
-      ...createGqlResponseSchema,
-      response: {
-        200: gqlResponseSchema,
-      },
-    },
-    async handler(req) {
-      // return graphql();
-    },
+const plugin: FastifyPluginAsync = async (fastify) => {
+  fastify.decorate('userLoader', createUserLoader(fastify.prisma));
+
+  fastify.post('/', async (request, reply) => {
+    fastify.log.info('GraphQL endpoint hit');
+    const { query, variables } = request.body as { query: string; variables?: Record<string, any> };
+
+    const result = await graphql({
+      schema,
+      source: query,
+      variableValues: variables,
+      contextValue: { prisma: fastify.prisma, userLoader: fastify.userLoader },
+    });
+
+    reply.send(result);
   });
 };
 
